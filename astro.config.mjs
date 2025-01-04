8f605bdcc6c230c367444169a3dbe7773f81e3be
Hayoun Kim <hayoun.k.1102@gmail.com>
2025-01-03 17:00:54 -0800

    astro config base

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://hayoun-k.github.io/cmstest',
-  //base: 'cmstest',
+  base: 'cmstest',
   srcDir: './src',
   publicDir: './public'
 });
