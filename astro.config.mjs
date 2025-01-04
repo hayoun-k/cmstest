0af68a587d005626cc64c763204d2dff09da9e51
Hayoun Kim <hayoun.k.1102@gmail.com>
2025-01-03 17:06:44 -0800

    astro config typo removed

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://hayoun-k.github.io/',
+  site: 'https://hayoun-k.github.io',
   base: '/cmstest',
   srcDir: './src',
   publicDir: './public'
