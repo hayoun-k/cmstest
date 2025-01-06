a8b6aacd61b3b0ad97baf99d6c5e4dbb788eb956
Hayoun Kim <hayoun.k.1102@gmail.com>
2025-01-06 15:17:04 -0800

    change hosting to cloudflare. astro config modified

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://hayoun-k.github.io',
-  base: '/cmstest',
   srcDir: './src',
   publicDir: './public'
 });
