026d06cc6086b3f30a95942f70b6065198273689
Hayoun Kim <hayoun.k.1102@gmail.com>
2025-02-23 18:05:44 -0800

    recipe

--- src/content.config.ts
+++ src/content.config.ts
@@ -14,4 +14,19 @@ const blog = defineCollection({
   }),
 });
 
-export const collections = { blog };
+//export const collections = { blog };
+
+const recipe = defineCollection({
+  loader: glob({ pattern: "**/[^_]*.md", base: "./src/content/recipe" }),
+  schema: z.object({
+    title: z.string(),
+    publishedAt: z.date(),
+    description: z.string(),
+    //isPublish: z.boolean(),
+    //isDraft: z.boolean().default(false),
+    author: z.string(),
+    cooking_time: z.string(),
+  }),
+});
+
+export const collections = { blog, recipe };
